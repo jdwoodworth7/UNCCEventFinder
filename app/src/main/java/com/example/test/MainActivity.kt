b24a3a07aa15a4a505941d1af1861640f631569a
jdwoodworth7 <jdwoodworth7@gmail.com>
2023-10-23 20:27:33 -0400

    Initial commit

---
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.graphics.Paint
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val SignUpUnderline = findViewById<TextView>(R.id.SignUpUnderline)
+        SignUpUnderline.setOnClickListener()
+    }
+}
