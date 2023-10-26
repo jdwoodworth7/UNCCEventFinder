e8f896c59c4d93e913e172ac1c37ebcf8fb5aef0
jdwoodworth7 <jdwoodworth7@gmail.com>
2023-10-25 21:32:17 -0400

    Login and Register functionality

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -4,12 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.graphics.Paint
+import android.content.Intent
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val SignUpUnderline = findViewById<TextView>(R.id.SignUpUnderline)
-        SignUpUnderline.setOnClickListener()
+        val EmailButton = findViewById<Button>(R.id.EmailButton)
+        SignUpUnderline.setOnClickListener{
+            startActivity(Intent(this@MainActivity, RegisterActivity::class.java))
+        }
+
+        EmailButton.setOnClickListener{
+            startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+        }
+
     }
 }
