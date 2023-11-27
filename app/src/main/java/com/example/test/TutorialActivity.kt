94d938868fb69f8cab8f04eaaa53b878d5c306ad
Andrew-Pavlovski <Andrewcpavlovski@gmail.com>
2023-11-27 09:44:03 -0500

    Basic Functionality of Tutorial
    Using images of the app and placing buttons that I will later make transparent with no text that can help the user click where they need to. The cloud image will probably change to something else.

--- app/src/main/java/com/example/test/TutorialActivity.kt
+++ app/src/main/java/com/example/test/TutorialActivity.kt
@@ -1,47 +1,75 @@
 package com.example.test
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
 
-class TutorialActivity : AppCompatActivity() {
-
-    private val tutorialImages = arrayOf(
-        R.drawable.tutorial_step_1,
-        R.drawable.tutorial_step_2,
-        R.drawable.tutorial_step_3
-        // Add more tutorial steps as needed
-    )
-
-    private var currentStep = 0
+class TutorialActivity : AppCompatActivity(), View.OnClickListener {
+    private var step1Button: Button? = null
+    private var step2Button: Button? = null
+    private var step3Button: Button? = null
+    private var tutorialImageView: ImageView? = null
+    private var currentStep = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tutorial_layout)
 
-        val tutorialImageView: ImageView = findViewById(R.id.tutorialImageView)
-        val nextButton: Button = findViewById(R.id.nextButton)
+        // Initialize your buttons, image view, and set initial visibility
+        step1Button = findViewById<Button>(R.id.step1Button)
+        step2Button = findViewById<Button>(R.id.step2Button)
+        step3Button = findViewById<Button>(R.id.step3Button)
+        tutorialImageView = findViewById<ImageView>(R.id.tutorialImageView)
+        updateVisibility()
 
-        // Initial setup
-        tutorialImageView.setImageResource(tutorialImages[currentStep])
+        // Set click listeners using the safe call operator
+        step1Button?.setOnClickListener(this)
+        step2Button?.setOnClickListener(this)
+        step3Button?.setOnClickListener(this)
+    }
 
-        nextButton.setOnClickListener {
-            // Handle the next button click
-            if (currentStep < tutorialImages.size - 1) {
-                // If there are more steps, move to the next step
-                currentStep++
-                tutorialImageView.setImageResource(tutorialImages[currentStep])
-            } else {
-                // If it's the last step, finish the tutorial or take appropriate action
-                finishTutorial()
-            }
+    override fun onClick(view: View) {
+        // Handle button clicks
+        currentStep++
+        if (currentStep <= 3) {
+            updateVisibility()
+            updateImage()
+        } else {
+            // If it's the last step, go back to MainActivity
+            val intent = Intent(this@TutorialActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 
-    private fun finishTutorial() {
-        // Implement logic for finishing the tutorial
-        // This might include saving a preference to indicate that the user has completed the tutorial
-        finish()
+    private fun updateVisibility() {
+        // Update visibility based on the current step
+        findViewById<FrameLayout>(R.id.step1FrameLayout)?.visibility =
+            if (currentStep == 1) View.VISIBLE else View.INVISIBLE
+        findViewById<FrameLayout>(R.id.step2FrameLayout)?.visibility =
+            if (currentStep == 2) View.VISIBLE else View.INVISIBLE
+        findViewById<FrameLayout>(R.id.step3FrameLayout)?.visibility =
+            if (currentStep == 3) View.VISIBLE else View.INVISIBLE
+
+        // Update buttons visibility based on the current step
+        step1Button?.visibility = if (currentStep == 1) View.VISIBLE else View.INVISIBLE
+        step2Button?.visibility = if (currentStep == 2) View.VISIBLE else View.INVISIBLE
+        step3Button?.visibility = if (currentStep == 3) View.VISIBLE else View.INVISIBLE
+    }
+
+    private fun updateImage() {
+        // Update the tutorial image based on the current step
+        tutorialImageView?.setImageResource(
+            when (currentStep) {
+                1 -> R.drawable.tutorial_step_1
+                2 -> R.drawable.tutorial_step_2
+                3 -> R.drawable.tutorial_step_3
+                else -> R.drawable.baseline_image_24 // Change to a default image or handle accordingly
+            }
+        )
     }
 }
