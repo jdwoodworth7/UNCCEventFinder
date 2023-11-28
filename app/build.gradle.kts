43262ae3f22e38042b0af12cf43d9b915b409eca
jdwoodworth7 <jdwoodworth7@gmail.com>
2023-11-27 20:05:23 -0500

    Moved calendar view to my schedule, made register interact with firebase, made calendarviewactivity read from firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.firebase:firebase-database-ktx:20.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/test/RegisterActivity.kt
+++ app/src/main/java/com/example/test/RegisterActivity.kt
@@ -3,15 +3,35 @@ package com.example.test
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.Firebase
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
+import com.google.firebase.storage.FirebaseStorage
+
 
 class RegisterActivity : AppCompatActivity() {
+
+
+    private val db = FirebaseFirestore.getInstance()
+    private val storage = FirebaseStorage.getInstance()
+    private val storageRef = storage.reference
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        FirebaseApp.initializeApp(this)
+
         setContentView(R.layout.activity_register)
         val BackButton: ImageButton = findViewById(R.id.BackButton)
         BackButton.setOnClickListener {
@@ -25,12 +45,64 @@ class RegisterActivity : AppCompatActivity() {
         val password: EditText = findViewById(R.id.editPassword)
         val signUpButton: Button = findViewById(R.id.SignUpButton)
 
+
+
+        //val userRef: DatabaseReference = FirebaseDatabase.getInstance().getReference().child("UserData")
+
+
         signUpButton.setOnClickListener {
             if (TextUtils.isEmpty(firstName.text.toString()) || TextUtils.isEmpty(lastName.text.toString()) || TextUtils.isEmpty(email.text.toString()) || TextUtils.isEmpty(password.text.toString())) {
                 Toast.makeText(this, "Please fill out all of the prompts", Toast.LENGTH_SHORT).show()
             } else {
+                val firstNameStr: String = firstName.text.toString()
+                val lastNameStr: String = lastName.text.toString()
+                val emailStr: String = email.text.toString()
+                val passwordStr: String = password.text.toString()
+                val blankTemp: String = ""
+                val blankList = emptyList<String>()
+
+                saveUserDataToFirestore(
+                    emailStr ?: "",
+                    firstNameStr ?: "",
+                    lastNameStr ?: "",
+                    passwordStr ?: "",
+                    blankTemp ?: "",
+                    blankList ?: emptyList(),
+                )
                 startActivity(Intent(this@RegisterActivity, MainActivity::class.java))
             }
         }
+    }
+
+    private fun saveUserDataToFirestore(
+        email: String,
+        firstname: String,
+        lastname: String,
+        password: String,
+        status: String,
+        friendids: List<String>
+    ) {
+        // Create a new event document in the "Users" collection
+        val user = hashMapOf(
+            "email" to email,
+            "firstname" to firstname,
+            "lastname" to lastname,
+            "password" to password,
+            "status" to status,
+            "friendids" to friendids
+        )
+
+        // Add the event to the "Users" collection
+        db.collection("Users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d("Firestore", "DocumentSnapshot added with ID: ${documentReference.id}")
+
+                // Finish the current activity to remove it from the back stack
+                finish()
+            }
+            .addOnFailureListener { e ->
+                Log.e("Firestore", "Error adding event document", e)
+            }
     }
 }
