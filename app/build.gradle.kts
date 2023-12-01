dd65db778d8e67d8d286450f573b7764daf66126
Derek <dwang26@uncc.edu>
2023-12-01 17:25:42 -0500

    Created Setting page and privacy settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -54,6 +55,10 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.firebase:firebase-auth-ktx:22.3.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.google.firebase:firebase-database-ktx:20.3.0")
+    implementation("com.google.firebase:firebase-auth:22.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -61,10 +66,21 @@ dependencies {
     //noinspection GradleDependency
     implementation("androidx.activity:activity-ktx:$1.7.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
-    implementation("com.google.android.libraries.places:places:3.2.0")
-    implementation ("io.coil-kt:coil:1.4.0")
-    implementation ("com.squareup.okhttp3:okhttp:4.11.0")
-    implementation ("com.google.code.gson:gson:2.8.9")
-    implementation ("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.libraries.places:places:3.3.0")
     implementation("io.coil-kt:coil:1.4.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.google.code.gson:gson:2.10")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.squareup.picasso:picasso:2.8")
+    implementation("com.google.maps.android:android-maps-utils:3.8.0")
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    // Declare the dependency for the Cloud Firestore library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-storage")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-database-ktx")
+    implementation("com.google.firebase:firebase-auth-ktx")
+
+
 }
